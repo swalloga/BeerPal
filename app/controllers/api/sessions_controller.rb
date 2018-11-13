0b713317ef4c9e15236ee2fab3c092f672fcabe5
Sarah Walloga <swalloga@gmail.com>
2018-11-13 18:06:04 -0500

    comment in sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,24 +1,23 @@
 class Api::SessionsController < ApplicationController
-  # def create
-  #   @user = User.find_by_credentials(
-  #     params[:user][:username],
-  #     params[:user][:password]
-  #   )
-  #   if @user
-  #     log_in!(@user)
-  #     render "api/users/show"
-  #   else
-  #     render json: ['invalid credentails'], status: 404
-  #   end
-  # end
-  #
-  # def destroy
-  #   if logged_in?
-  #     log_out!
-  #     render json: {}
-  #   else
-  #     render json: ["not logged in"], status: 404
-  #   end
-  # end
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      log_in!(@user)
+      render "api/users/show"
+    else
+      render json: ['invalid credentails'], status: 404
+    end
+  end
 
+  def destroy
+    if logged_in?
+      log_out!
+      render json: {}
+    else
+      render json: ["not logged in"], status: 404
+    end
+  end
 end
