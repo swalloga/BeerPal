00a01859801e3aee37065a3bd50e0f3d393c0429
Sarah Walloga <swalloga@gmail.com>
2018-11-15 09:47:15 -0500

    fix username uniqueness validation and background image tag

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@
 
 class User < ApplicationRecord
   validates :username, :email, :name, :beer_allowance, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
 
   after_initialize :ensure_session_token
